2718f07a513f4e1197f6a38ee76529bf9012e188
kervinazo3 <kervinazo3@gmail.com>
2023-08-15 10:59:43 -0400

    text

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,8 +5,8 @@ import { BsFillPatchCheckFill } from "react-icons/bs";
 const Experience = () => {
   return (
     <section id="experience">
-      <h5>What Skills I have</h5>
-      <h2>my experience</h2>
+      <h5>¿Qué habilidades tengo?</h5>
+      <h2>Mis experiencias</h2>
 
       <div className="container experience__container">
         <div className="experience__frontend">
